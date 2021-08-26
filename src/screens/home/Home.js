0c2770c17d2a9364b6e6d80900c675d3c2592ffb
sindhushekar95 <sindhushekar.dsd25@iiitb.net>
2021-08-26 20:52:35 +0530

    Cleanup and set up base for filters

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -6,11 +6,13 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Header from '../../common/header/Header';
 import { useHistory } from 'react-router-dom';
 import './Home.css';
@@ -20,10 +22,15 @@ const Home = (props) => {
     const baseUrl = "http://localhost:8085/api/v1/";
     const [upcomingMovies, setUpcomingMovies] = useState([]);
     const [releasedMovies, setReleasedMovies] = useState([]);
+    const [genres, setGenres] = useState([]);
+    const [artists, setArtists] = useState([]);
     const DATE_OPTIONS = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
     const history = useHistory();
     const handleMovieClick = (id) => history.push(`/details/${id}`);
-
+    const [openMenu, setMenuOpen] = useState();
+    const handleOpenState = () => {
+        setMenuOpen(true);
+    }
     useEffect(() => {
 
         fetch(baseUrl + "movies?limit=50", {
@@ -36,6 +43,28 @@ const Home = (props) => {
             .then(response => {
                 setUpcomingMovies(response.movies.filter(res => res.status === 'PUBLISHED'));
                 setReleasedMovies(response.movies.filter(res => res.status === 'RELEASED'));
+            })
+
+        fetch(baseUrl + "genres", {
+            method: "GET",
+            headers: {
+                "Accept": "application/json;charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                setGenres(response.genres);
+            })
+
+        fetch(baseUrl + "artists?limit=50", {
+            method: "GET",
+            headers: {
+                "Accept": "application/json;charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                setArtists(response.artists);
             })
     }, []);
 
@@ -70,39 +99,38 @@ const Home = (props) => {
                     </GridList>
                 </div>
                 <div className="movieFilter">
-                    <Card variant="outlined" >
+                    <Card variant="outlined">
                         <CardContent>
                             <Typography color="textSecondary">
                                 FIND MOVIES BY:
                             </Typography>
-                            <form className="dFlex alignCenter justifyCenter flexColumn p15">
-                                <div className="mb15">
-                                    <TextField id="moviename" label="Movie Name" />
-                                </div>
-                                <div className="mb15">
-                                    <FormControl>
-                                        <InputLabel id="demo-simple-select-label">Genres</InputLabel>
-                                        <Select
-                                            id="demo-simple-select"
-                                        >
-                                            <MenuItem value={10}>Ten</MenuItem>
-                                            <MenuItem value={20}>Twenty</MenuItem>
-                                            <MenuItem value={30}>Thirty</MenuItem>
-                                        </Select>
-                                    </FormControl>
-                                </div>
-                                <div className="mb15">
-                                    <FormControl>
-                                        <InputLabel id="demo-simple-select-label">Artist</InputLabel>
-                                        <Select
-                                            id="demo-simple-select"
-                                        >
-                                            <MenuItem value={10}>Ten</MenuItem>
-                                            <MenuItem value={20}>Twenty</MenuItem>
-                                            <MenuItem value={30}>Thirty</MenuItem>
-                                        </Select>
-                                    </FormControl>
-                                </div>
+                            <form noValidate className="dFlex alignCenter justifyCenter flexColumn">
+                                <FormControl className="formControl w100">
+                                    <InputLabel htmlFor="movie">Movie</InputLabel>
+                                    <Input id="movie" />
+                                </FormControl>
+                                <FormControl className="formControl w100">
+                                    <InputLabel id="genres">Genres</InputLabel>
+                                    <Select
+                                        labelId="genres"
+                                        id="genres-dropdown"
+                                        open={openMenu} onChange={handleOpenState}
+                                    >{genres.map((res, i) => (
+                                        <MenuItem key={i}><FormControlLabel key={res.id} control={<Checkbox name={res.genre} />} label={res.genre} /></MenuItem>
+                                    ))}
+                                    </Select>
+                                </FormControl>
+                                <FormControl className="formControl w100">
+                                    <InputLabel id="artists">Artists</InputLabel>
+                                    <Select
+                                        labelId="artists"
+                                        id="artists-dropdown"
+                                        open={openMenu} onChange={handleOpenState}
+                                    >{artists.map((res, i) => (
+                                        <MenuItem key={i}><FormControlLabel key={res.id} control={<Checkbox name={`${res.first_name} ${res.last_name}`} />} label={`${res.first_name} ${res.last_name}`} /></MenuItem>
+                                    ))}
+                                    </Select>
+                                </FormControl>
                                 <Button variant="contained" color="primary">APPLY</Button>
                             </form>
                         </CardContent>
